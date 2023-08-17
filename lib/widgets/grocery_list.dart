486aa13211edc09ee0f335eb95516182d828c319
Abhinav Saurav <abhinav.saurav@thesmartq.com>
2023-08-17 23:19:24 +0530

    grocery list configured for dynamic list

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,28 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/models/grocery_item.dart';
+import 'package:shopping_list_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  List<GroceryItem> _groceryItems = [];
+
+  void _addNewItem() async {
+    // if await is not added we will get errors below
+    final result = await Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        return const NewItem();
+      },
+    ));
+
+    if (result == null) return;
+    setState(() {
+      _groceryItems.add(result);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your grocery list"),
-      ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
+    Widget content;
+    if (_groceryItems.isEmpty) {
+      content = const Center(
+        child: Text("No data found"),
+      );
+    } else {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: groceryItems[index].category.colordata,
+          return Dismissible(
+            key: ValueKey(_groceryItems[index].id),
+            onDismissed: (direction) {
+              setState(() {
+                _groceryItems.remove(_groceryItems[index]);
+
+                // _groceryItems = _groceryItems
+                //     .where((element) => element.id != _groceryItems[index].id)
+                //     .toList();
+              });
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.colordata,
+              ),
+              trailing: Text(_groceryItems[index].quantity.toString()),
             ),
           );
         },
-      ),
-    );
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Your grocery list"),
+          centerTitle: false,
+          actions: [
+            IconButton(onPressed: _addNewItem, icon: const Icon(Icons.add))
+          ],
+        ),
+        body: content);
   }
 }
